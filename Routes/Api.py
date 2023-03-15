b22c0dd7fbb0f751ed94474eec9fd5602b1cfc1f
araschang0827 <b09704076@ntu.edu.tw>
2023-03-15 15:26:54 +0800

    add Yuan Indicator

--- Routes/Api.py
+++ Routes/Api.py
@@ -1,13 +1,17 @@
 from flask_restful import Api
 from flask import Flask
+from discord import SyncWebhook
 from apscheduler.schedulers.background import BackgroundScheduler
+from Base.ConfigReader import Config
 from Application.Api.Controller.WebsocketController import WebsocketController
 from Application.Api.Controller.IndicatorController import IndicatorController
 from Application.Indicators.YuanIndicator import YuanIndicator
 
 app = Flask(__name__)
 api = Api(app)
-scheduler = BackgroundScheduler(job_defaults={'max_instances': 2})
+scheduler = BackgroundScheduler(job_defaults={'max_instances': 3})
+config = Config()
+stable_check_webhook = config['Discord']['stable_check_webhook']
 
 def job_bitcoin_trade():
     indicator = YuanIndicator('BTC/USDT')
@@ -23,6 +27,9 @@ def job_eth_trade():
     indicator.checkSignal(mean_volume, ohlcv)
     print('JOB "ETH TRADE" DONE')
 
+def stable_check():
+    webhook = SyncWebhook.from_url(stable_check_webhook)
+    webhook.send('STABLE CHECK')
 
 api.add_resource(
     WebsocketController,
@@ -36,4 +43,5 @@ api.add_resource(
 
 scheduler.add_job(job_bitcoin_trade, 'interval', seconds=30)
 scheduler.add_job(job_eth_trade, 'interval', seconds=30)
+scheduler.add_job(stable_check, 'interval', hours=8)
 scheduler.start()
