331a467cb8b74d4fb64c1cb24f8c05a2a9bc9c2d
araschang0827 <b09704076@ntu.edu.tw>
2023-04-20 17:33:07 +0800

    fix bugs

--- Base/Api.py
+++ Base/Api.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 from flask import Flask
-from Indicators.Scheduler import YuanScheduler
+from Indicators.Scheduler import YuanScheduler, BTCHedgeScheduler
 from Model import Scheduler
 
 app = Flask(__name__)
